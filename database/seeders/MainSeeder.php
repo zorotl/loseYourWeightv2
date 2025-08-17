34715c95456f6b2fa8e219bffdb14d02cf9bb199
Martin Striednig <m.striednig@gmail.com>
2025-08-17 19:13:35 +0200

    Seeder anpassen, kein Dummy Food Fixes #29

--- database/seeders/MainSeeder.php
+++ database/seeders/MainSeeder.php
@@ -2,62 +2,36 @@
 
 namespace Database\Seeders;
 
-use App\Models\Food;
-use App\Models\Meal;
 use App\Models\User;
 use App\Models\WeightHistory;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Collection;
 
 class MainSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
-        // 1. Create all users first
-        User::factory()->create([
+        // 1. Create a main user you can log in with
+        $mainUser = User::factory()->create([
             'name' => 'Test User',
             'email' => 'info@stws.ch',
         ]);
 
-        User::factory(5)->create();
+        // Add some weight history to the main user
+        WeightHistory::factory(20)->for($mainUser)->create();
 
-        // 2. Then get a clean collection of all created users
-        $users = User::all();
+        // 2. Create a few other random users
+        User::factory(5)->create()->each(function ($user) {
+            WeightHistory::factory(10)->for($user)->create();
+        });
 
-        // Prepare data arrays in memory
-        $weightHistories = [];
-        $meals = [];
-        $foods = [];
-        $foodMealPivots = [];
+        $this->command->info('Test users and weight histories created.');
 
-        foreach ($users as $user) {
-            // Prepare weight histories for this user
-            for ($i = 0; $i < 20; $i++) {
-                $weightHistories[] = WeightHistory::factory()->make(['user_id' => $user->id])->toArray();
-            }
-
-            // Prepare meals and their ingredients
-            $userMeals = Meal::factory(3)->make(['user_id' => $user->id]);
-            foreach ($userMeals as $meal) {
-                // We have to save the meal to get an ID for the pivot table
-                $meal->save();
-
-                // Create foods for this meal
-                $mealFoods = Food::factory(5)->create(['creator_id' => $user->id]);
-
-                // Prepare pivot data
-                foreach ($mealFoods as $food) {
-                    $foodMealPivots[] = [
-                        'meal_id' => $meal->id,
-                        'food_id' => $food->id,
-                        'quantity_grams' => rand(50, 250)
-                    ];
-                }
-            }
-        }
-
-        // Bulk insert all prepared data in single queries
-        WeightHistory::insert($weightHistories);
-        \DB::table('food_meal')->insert($foodMealPivots);
+        // 3. Call the Artisan command to import real base foods
+        $this->command->info('Starting import of base foods from API...');
+        $this->command->call('import:base-foods');
+        $this->command->info('Base foods imported.');
     }
 }
