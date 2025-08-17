d8429a4602a3ece75f8494db2ee999dd7109074a
Martin Striednig <m.striednig@gmail.com>
2025-08-17 10:59:08 +0200

    Eigene Seite für Tagesverbrauch Fixes #3

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,6 @@
 import flatpickr from "flatpickr";
+import { German } from "flatpickr/dist/l10n/de.js";
+window.German = German;
 
 import { Chart, registerables } from 'chart.js';
 import 'chartjs-adapter-date-fns';
